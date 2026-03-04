fd11ecd0c3cee45a2a8384e36d077063e3095c81
andreeavilcu <andreea.vilcu@student.unitbv.ro>
2026-03-04 21:39:19 +0200

    US5: Clear and specific error messages (including county validation)

--- validare_cnp.py
+++ validare_cnp.py
@@ -2,6 +2,22 @@ import tkinter as tk
 from tkinter import font as tkfont
 import calendar
 
+COUNTIES = {
+        "01":"Alba","02":"Arad","03":"Arges","04":"Bacau","05":"Bihor",
+        "06":"Bistrita-Nasaud","07":"Botosani","08":"Brasov","09":"Braila",
+        "10":"Buzau","11":"Caras-Severin","12":"Cluj","13":"Constanta",
+        "14":"Covasna","15":"Dambovita","16":"Dolj","17":"Galati","18":"Gorj",
+        "19":"Harghita","20":"Hunedoara","21":"Ialomita","22":"Iasi","23":"Ilfov",
+        "24":"Maramures","25":"Mehedinti","26":"Mures","27":"Neamt","28":"Olt",
+        "29":"Prahova","30":"Satu Mare","31":"Salaj","32":"Sibiu","33":"Suceava",
+        "34":"Teleorman","35":"Timis","36":"Tulcea","37":"Vaslui","38":"Valcea",
+        "39":"Vrancea","40":"Bucharest","41":"Bucharest S1","42":"Bucharest S2",
+        "43":"Bucharest S3","44":"Bucharest S4","45":"Bucharest S5","46":"Bucharest S6",
+        "51":"Calarasi","52":"Giurgiu"
+    }
+
+WEIGHTS = [2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9]
+
 def validate_cnp(raw):
     cnp = raw.strip()
 
@@ -18,6 +34,8 @@ def validate_cnp(raw):
     aa = int(cnp[1:3])
     ll = int(cnp[3:5])
     zz = int(cnp[5:7])
+    jj = cnp[7:9]
+    c  = int(cnp[12])
 
     if s < 1 or s > 9:
         return False, "Invalid CNP: first digit (sex) must be between 1 and 9."
@@ -33,10 +51,8 @@ def validate_cnp(raw):
     if day < 1 or day > max_day:
         return False, f"Invalid CNP: birth day ({day}) is not valid for month {month:02d}/{year}."
 
-    WEIGHTS = [2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9]
-    jj = cnp[7:9]
-    c  = int(cnp[12])
-
+    if jj not in COUNTIES:
+        return False, f"CNP invalid: codul județului ({jj}) nu este valid.", None
     total = sum(int(cnp[i]) * WEIGHTS[i] for i in range(12))
     remainder = total % 11
     expected = 1 if remainder == 10 else remainder
